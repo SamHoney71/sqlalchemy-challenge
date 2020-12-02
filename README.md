2489e7ee095daa58204c4ae0886008089a00a6bf
SamHoney71 <gjeter91@gmail.com>
2020-12-01 19:56:36 -0600

    Website for JSON API & Jupyter Notebook Analysis works - Bonus not done

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # sqlalchemy-challenge - Jeter
 
-1.  climate_jeter.ipynb performs the following functions:
+1.  climate_jeter.ipynb conducts a dataframe and graphical analysis of the Hawai Weather Stations
 
-2.  app.yy perfroms the following functions:
+
+2.  app.py makes the Hawaii Weather Stations database available via APIs in JSON format.
+
+Homepage is:  http://127.0.0.1:5000/
+
+Available routes:
+
+/api/v1.0/percipitation
+
+/api/v1.0/stations
+
+/api/v1.0/tobs
+
+/api/v1.0/start_date/ (Note: Enter start date after'/' in YYYY-MM-DD format.)
+
+/api/v1.0/date_range// (Note: Enter date range format: start date/end date (i.e. YYYY-MM-DD/YYYY-MM-DD))
 
 
 
--- app.py
+++ app.py
@@ -28,6 +28,8 @@ Station = Base.classes.station
 session = Session(engine)
 
 # Save references to perciptiation & date table & convert to dictionary
+# reference for syntax to convert df to dictionary:  https://stackoverflow.com/questions/43401677/pandas-convert-dataframe-columns-into-dict-with-col-title-as-dict-key-and-col-v
+
 date_prcp_df = pd.read_sql("SELECT date, prcp FROM Measurement", conn)
 date_prcp_dic = date_prcp_df.to_dict('records')
 
@@ -53,6 +55,7 @@ hi_station_temp_df =pd.DataFrame(hi_station_12mo_temp)
 hi_station_temp_df['date'] = pd.to_datetime(hi_station_temp_df['date'])
 hi_station_temp_dic = hi_station_temp_df.to_dict('records')
 
+
 #################################################
 # Flask Setup
 #################################################
@@ -73,10 +76,10 @@ def welcome():
         f"/api/v1.0/stations<br/>"
         f"<br/>"
         f"/api/v1.0/tobs<br/><br/>"
-        f"/api/v1.0/start_date/<date>"
+        f"/api/v1.0/start_date/<start_date>"
         f" (Note: Enter start date after'/' in YYYY-MM-DD format.) <br/> <br/>"
-        f"/api/v1.0/start_date/end_date/<end_date> "
-        f"   (Note: Enter start date after'/' and then end date after'/' in YYYY-MM-DD format.)<br/>"
+        f"/api/v1.0/date_range/<start_date>/<end_date> "
+        f"   (Note: Enter date range format: start date/end date (i.e. YYYY-MM-DD/YYYY-MM-DD))<br/>"
     )
 
 # Percipitation & dates API Page
@@ -98,37 +101,39 @@ def hi_station_observed_temp():
     
      return jsonify(hi_station_temp_dic) 
 
-# @app.route("/api/v1.0/start_date/<date>")
-# def measurement_date (date):
-#     # """Return a JSON list of the minimum temperature, the average temperature, 
-#     # and the max temperature for a given start or start-end range."""
-#     # """When given the start only, 
-#     # calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date."""
+@app.route("/api/v1.0/start_date/<start_date>")
+def temp_analysis_by_start_date (start_date):
+    #  When given the start only, calculate TMIN, TAVG, and TMAX#
+    #  for all dates greater than and equal to the start date. & return JSON
+    #Acknowledgement:  Michael Badinger helped me in this area by explaining the ( def xxxxx (yyyy):  ) function & syntax
 
-#     canonicalized = date.replace()
-#     for date in measurement_df:
-#         search_term = measurement_dic[{"date"].replace()
+    session = Session(engine)
 
-#         if search_term == canonicalized:
-#             return jsonify(measurement_dic)
+    sel_3 = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
 
-#     return jsonify({"error": f"Start date {date} not found."}), 404   
+    temp_values = session.query(*sel_3).\
+        filter(Measurement.date >= start_date).all()
 
-# @app.route("/api/v1.0/start_date/end_date/<end_date>")
-# def vacation start date (start):
-#     """Return a JSON list of the minimum temperature, the average temperature, 
-#     and the max temperature for a given start or start-end range."""
-#     """When given the start and the end date, calculate the TMIN, TAVG, 
-#     and TMAX for dates between the start and end date inclusive."""
+    return (jsonify(temp_values))
+   
 
-#     canonicalized = start.replace(" ", "").lower()
-#     for xxx in xxx:
-#         xx = xx["start"].replace(" ", "").lower()
+@app.route("/api/v1.0/date_range/<start_date>/<end_date>")
+def date_range (start_date, end_date):
 
-#         if search_term == canonicalized:
-#             return jsonify(xxx)
+# Return a JSON list of the minimum temperature, the average temperature,#
+#  and the max temperature for a given start-end range."""
+#Acknowledgement:  Michael Badinger helped me in this area by explaining the ( def xxxxx (yyyy):  ) function & syntax
 
-#     return jsonify({"error": f"Start date {start} not found."}), 404  
+
+    session = Session(engine)
+
+    sel_4 = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    temp_values_2 = session.query(*sel_4).\
+        filter(Measurement.date >= start_date).\
+        filter(Measurement.date <= end_date).all()
+
+    return (jsonify(temp_values_2))
 
 
 # 4. Define main behavior
